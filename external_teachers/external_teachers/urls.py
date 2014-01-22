311d4505531d63d76273c593f7a8568e6768661c
Samuel Coelho <samuelfcmc@gmail.com>
2014-01-22 17:28:01 +0000

    Created django project

---
+++ external_teachers/external_teachers/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'external_teachers.views.home', name='home'),
+    # url(r'^external_teachers/', include('external_teachers.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
