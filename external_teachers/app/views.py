ee7a9049306d24ecd236cdcffe4050be7d318718
Samuel Coelho <samuelfcmc@gmail.com>
2014-01-22 22:59:12 +0000

    Created a index view

--- external_teachers/app/views.py
+++ external_teachers/app/views.py
@@ -1,3 +1,11 @@
+#App views
+
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+# Entry point
+def index(request):
+	context = {}
+	return render(request, 'app/index.html', context)
+
