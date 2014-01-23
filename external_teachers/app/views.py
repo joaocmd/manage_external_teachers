1db1b61773fb7f689dc180a2ab1add25e8b774c6
Samuel Coelho <samuelfcmc@gmail.com>
2014-01-23 19:24:02 +0000

    Implemented login functionality

--- external_teachers/app/views.py
+++ external_teachers/app/views.py
@@ -4,16 +4,42 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+
 import fenix
 
 import os
 
 fenixAPI = fenix.FenixAPISingleton()
+def_password = '0'
 
 # Entry point
 def index(request):
 	url = fenixAPI.get_authentication_url()
-	context = {'auth_url': url}
+	code = request.GET.get('code')
+	login = False
+	name = ''
+
+	if code and not request.user.is_authenticated():
+		fenixAPI.set_code(code)
+		person = fenixAPI.get_person()
+		username = person['istId']
+		email = person['email']
+		user = authenticate(username=username, password=def_password)
+		#User doesn't exist
+		if user is None:
+			#Create the user
+			user = User.objects.create_user(username, email, def_password)
+			user = authenticate(username=username, password=def_password)
+
+		if user is not None:
+			if user.is_active:
+				login = True
+				name = person['name']
+
+	context = {'auth_url': url, 'login' : login, 'name' : name}
+
 	return render(request, 'app/index.html', context)
 
 def about(request):
