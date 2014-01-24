79eb787e2b46f18661e51c2854d1da3e3bb2ac9d
Samuel Coelho <samuelfcmc@gmail.com>
2014-01-24 18:54:51 +0000

    Changed model attribute

--- external_teachers/app/models.py
+++ external_teachers/app/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 
 class ExternalTeacher(models.Model):
-	state = models.BooleanField()
+	is_closed = models.BooleanField()
 	close_date = models.DateTimeField('close date', null=True)
 	##TODO: Add more things...
