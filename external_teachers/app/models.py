657fa7868b8fbfd66e484f8a74caa69303621a42
Samuel Coelho <samuelfcmc@gmail.com>
2014-01-24 18:31:39 +0000

    Changed model name

--- external_teachers/app/models.py
+++ external_teachers/app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Item(models.Model):
+class ExternalTeacher(models.Model):
 	state = models.BooleanField()
 	close_date = models.DateTimeField('close date', null=True)
 	##TODO: Add more things...
