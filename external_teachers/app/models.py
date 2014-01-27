3378e8b51a3d9d7b60369c46b4d5cc544c9cde1c
Samuel Coelho <samuelfcmc@gmail.com>
2014-01-27 18:44:46 +0000

    Added extra fields to external teacher model

--- external_teachers/app/models.py
+++ external_teachers/app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.forms import ModelForm, Textarea
 
 class Profile(models.Model):
 	user = models.OneToOneField(User)
@@ -7,6 +8,23 @@ class Profile(models.Model):
 
 
 class ExternalTeacher(models.Model):
+	user = models.ForeignKey(User)
 	is_closed = models.BooleanField()
 	close_date = models.DateTimeField('close date', null=True)
-	##TODO: Add more things...
+	professional_category = models.CharField(max_length=200, blank=False)
+	hours_per_week = models.DecimalField(decimal_places=2, max_digits=5)
+	park = models.BooleanField()
+	card = models.BooleanField()
+	department = models.CharField(max_length=200)
+	name = models.CharField(max_length=200)
+	degree = models.CharField(max_length=200)
+	course = models.CharField(max_length=200)
+	course_manager = models.CharField(max_length=200)
+	notes = models.CharField(max_length=200, blank=True)
+
+class ExternalTeacherForm(ModelForm):
+	class Meta:
+		model = ExternalTeacher
+		fields = '__all__'
+		exclude = ('close_date', )
+		widgets = {'notes' : Textarea(), }
